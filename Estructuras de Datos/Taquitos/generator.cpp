71b388e112c14f24d98a210da901ca74efccb98f
Anas Iqbal <mohd.abd.6602@gmail.com>
2025-03-23 17:00:25 +0530

    remove extra sections

--- Estructuras de Datos/Taquitos/generator.cpp
+++
@@ -1,1 +0,0 @@
-tests/solutions/solution.cpp
--- Ordenamiento y Complejidad/Quicksort/quicksort.cpp
+++
@@ -1,9 +0,0 @@
-#include "quicksort.h"
-
-// int ElementoEnPosicion(int i);
-// bool Intercambiar(int i, int j);
-
-int HacerParticion(int inicio, int fin) {
-  // FIXME
-  return 0;
-}
